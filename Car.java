447d355cf6b67968e31273ae910665753d04dbd7
Dohryk <mars207@mail.ru>
2016-05-06 13:13:56 +0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -17,12 +17,53 @@ public class Car {
         car.printInfo();
     }
 
+    Car(){
+        model = "Mers";
+        year = 2015;
+        color = Color.BROWN;
+        max_speed = 150;
+        cost = 154.00;
+    }
+
     Car(String model, int year, Color color, int max_speed, double cost){
         this.model = model;
         this.color = color;
         this.year = year;
         this.max_speed = max_speed;
+
         this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getMax_speed() {
+        return max_speed;
+    }
+
+    public void setMax_speed(int max_speed) {
+        this.max_speed = max_speed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 
     public void printInfo(){
