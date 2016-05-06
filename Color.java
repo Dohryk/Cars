3ed746a2087a3031189d4d076ee7a832da6986f0
Dohryk <mars207@mail.ru>
2016-05-06 13:08:25 +0300

    Create Color.java

---
+++ Color.java
@@ -0,0 +1,13 @@
+package Cars;
+
+/**
+ * Created by vdohryk on 06.05.2016.
+ */
+public enum Color {
+    BLUE, RED, BLACK, GREY, DARK_GREY, YELLOW, PINK, GREEN, ORANGE, VIOLET, MAGENTA, BROWN, PLUM, WHITE, GOLD, METAL;
+
+    public Color getDefaultColor()
+    {
+        return BLACK;
+    }
+}
